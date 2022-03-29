314b5d0d9ae7e9bc2570e2c5a924c1659c46b1ce
Gonzalo Plaza <me@gonzaloplaza.com>
2022-03-29 20:18:27 +0200

    feat: update readme, update index msg

--- src/adapter/express/controller/getIndexController.ts
+++ src/adapter/express/controller/getIndexController.ts
@@ -4,7 +4,7 @@ export class GetIndexController {
   public invoke(req: Request, res: Response): Response {
     return res.json({
       success: true,
-      message: 'Hello Cognito Workshop',
+      message: 'Hello Cognito Workshop Test1',
       timestamp: +new Date()
     });
   }
